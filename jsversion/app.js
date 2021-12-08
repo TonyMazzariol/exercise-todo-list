eca6c18a97c8263f9dc226eb3f624a682357f612
Tony <Vous@exemple.com>
2021-12-08 22:52:36 +0100

    version_1.2

---
+++ jsversion/app.js
@@ -0,0 +1,26 @@
+let http = require('http');
+
+let fs = require('fs')
+
+let server  = http.createServer()
+
+server.on('request', (request, response) => {
+
+    fs.readFile('index.html', (err, data) => {
+        if (err) {
+            
+            response.writeHead(404)
+
+        } else {
+            response.writeHead(200, {
+        
+                'Content-type': 'text/html; charset=utf-8'
+            })
+        }
+        response.end(data)
+    })
+
+
+})
+
+server.listen(8000)
